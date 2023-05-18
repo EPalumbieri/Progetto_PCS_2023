c65caf2ef8e02b22698a69d23253990960d1a4cb
xenomaster10 <filippo.masotti10@gmail.com>
2023-05-18 13:32:45 +0200

    scheletro

--- Projects/Raffinamento/src/empty_class.cpp
+++ Projects/Raffinamento/src/empty_class.cpp
@@ -2,4 +2,317 @@
 
 namespace ProjectLibrary
 {
+    bool clockwise(const Point& P0,
+                   const Point& P1,
+                   const Point& P2)
+    {
+      return (P1.x - P0.x)*(P2.y - P0.y) - (P2.x - P0.x)*(P1.y - P0.y)>0;
+    }
+
+    double area(const Point& P1,
+                const Point& P2,
+                const Point& P3)
+    {
+        return abs((P1.x*(P2.y-P3.y)+P2.x*(P3.y-P1.y)+P3.x*(P1.y-P2.y))/2)
+    }
+
+    int GetEdgeFromPoints(int id1, int id2)
+    {
+        it = std::find_if(mesh.GraphedMesh.begin(), mesh.GraphedMesh.end(),
+        [&triangle](const OrientedEdge* edge) { return edge->RealEdge == triangle.edges[i]; });
+    }
+    // ***************************************************************************
+    bool ImportCell0Ds(TriangularMesh& mesh, string nomeFile)
+    {
+
+      ifstream file;
+      file.open(nomeFile);
+
+      if(file.fail())
+        return false;
+
+      list<string> listLines;
+      string line;
+      while (getline(file, line))
+        listLines.push_back(line);
+
+      file.close();
+
+      listLines.pop_front();
+
+      mesh.NumberCell0D = listLines.size();
+
+      if (mesh.NumberCell0D == 0)
+      {
+        cerr << "There is no cell 0D" << endl;
+        return false;
+      }
+
+
+      for (const string& line : listLines)
+      {
+        istringstream converter(line);
+
+        unsigned int id;
+        unsigned int marker;
+        Vector2d coord;
+        Point coordinates;
+        converter >>  id >> marker >> coord(0) >> coord(1);
+        coordinates.x=coord(0);
+        coordinates.y=coord(1);
+        mesh.Cell0D.insert(id,coordinates);
+
+        if( marker != 0)
+        {
+          if (mesh.Cell0DMarkers.find(marker) == mesh.Cell0DMarkers.end())
+            mesh.Cell0DMarkers.insert({marker, {id}});
+          else
+            mesh.Cell0DMarkers[marker].push_back(id);
+        }
+
+      }
+      file.close();
+      return true;
+    }
+
+    // ***************************************************************************
+    bool ImportCell1Ds(TriangularMesh& mesh, string nomeFile)
+    {
+
+      ifstream file;
+      file.open(nomeFile);
+
+      if(file.fail())
+        return false;
+
+      list<string> listLines;
+      string line;
+      while (getline(file, line))
+        listLines.push_back(line);
+
+      listLines.pop_front();
+
+      mesh.NumberCell1D = listLines.size();
+
+      if (mesh.NumberCell1D == 0)
+      {
+        cerr << "There is no cell 1D" << endl;
+        return false;
+      }
+
+
+      for (const string& line : listLines)
+      {
+        istringstream converter(line);
+
+        unsigned int id;
+        unsigned int marker;
+        Vector2i vertices;
+        Edge edge;
+
+        converter >>  id >> marker >> vertices(0) >> vertices(1);
+        edge.points.insert(vertices(0));
+        edge.points.insert(vertices(1));
+        mesh.Cell1D.insert(id,edge);
+
+        if( marker != 0)
+        {
+          if (mesh.Cell1DMarkers.find(marker) == mesh.Cell1DMarkers.end())
+            mesh.Cell1DMarkers.insert({marker, {id}});
+          else
+            mesh.Cell1DMarkers[marker].push_back(id);
+        }
+      }
+
+      file.close();
+
+      return true;
+    }
+    // ***************************************************************************
+    bool ImportCell2Ds(TriangularMesh& mesh,string nomeFile)
+    {
+
+      ifstream file;
+      file.open(nomeFile);
+
+      if(file.fail())
+        return false;
+
+      list<string> listLines;
+      string line;
+      while (getline(file, line))
+        listLines.push_back(line);
+
+      listLines.pop_front();
+
+      mesh.NumberCell2D = listLines.size();
+
+      if (mesh.NumberCell2D == 0)
+      {
+        cerr << "There is no cell 2D" << endl;
+        return false;
+      }
+
+
+
+      for (const string& line : listLines)
+      {
+        istringstream converter(line);
+
+        Triangle triangle;
+        array<OrientedEdge*,3> ptrs;
+        array<OrientedEdge,3> orientededges;
+        converter >> id;
+        for(unsigned int i = 0; i < 3; i++)
+          converter >> triangle.vertices[i];
+        for(unsigned int i = 0; i < 3; i++){
+          ptrs[i]=&orientededges[i];
+          converter >> triangle.edges[i];
+          ptrs[i]->RealEdge=triangle.edges[i];
+          ptrs[i]->RealTriangle=id;
+          it = std::find_if(mesh.GraphedMesh.begin(), mesh.GraphedMesh.end(),
+          [&triangle](const OrientedEdge* edge) { return edge->RealEdge == triangle.edges[i]; });
+
+          if(it != mesh.GraphedMesh.end()){
+             //found
+             ptrs[i]->symmetric=*it;
+             (*it)->symmetric=ptrs[i];
+          }else {
+            //not found
+
+          }
+        }
+        triangle.area=area(mesh.Cell0D[triangle.vertices[0]],mesh.Cell0D[triangle.vertices[1]],mesh.Cell0D[triangle.vertices[2]]);
+
+        unordered_set<unsigned int> temp01, temp02, temp12;
+        temp01.insert(triangle.vertices[0]);
+        temp01.insert(triangle.vertices[1]);
+        temp02.insert(triangle.vertices[0]);
+        temp02.insert(triangle.vertices[2]);
+        temp12.insert(triangle.vertices[1]);
+        temp12.insert(triangle.vertices[2]);
+        unsigned int e01, e02, e12;
+
+        if ((mesh.Cell1D[triangle.edge[0]].points==temp01)){
+            e01=0;
+        }else if (mesh.Cell1D[triangle.edge[1]].points==temp01){
+            e01=1;
+        }else{
+            e01=2;
+        }
+
+        if ((mesh.Cell1D[triangle.edge[0]].points==temp02)){
+            e02=0;
+        }else if (mesh.Cell1D[triangle.edge[1]].points==temp02){
+            e02=1;
+        }else{
+            e02=2;
+        }
+
+        if ((mesh.Cell1D[triangle.edge[0]].points==temp12)){
+            e12=0;
+        }else if (mesh.Cell1D[triangle.edge[1]].points==temp12){
+            e12=1;
+        }else{
+            e12=2;
+        }
+
+        if(clockwise(triangle.vertices[0], triangle.vertices[1], triangle.vertices[2])){
+            //e01->e12->e02
+            ptrs[e01]->next=ptrs[e12];
+            ptrs[e12]->next=ptrs[e02];
+            ptrs[e02]->next=ptrs[e01];
+        }else{
+           //e02->e12->e01
+            ptrs[e02]->next=ptrs[e12];
+            ptrs[e12]->next=ptrs[e01];
+            ptrs[e01]->next=ptrs[e02];
+        }
+
+        mesh.Cell2D.push_back(triangle);
+        for(int i=0;i<3;i++){
+           mesh.GraphedMesh.push_back(ptrs[i]);
+        }
+      }
+      file.close();
+      return true;
+    }
+
+    //***************************************************************************
+    bool ImportMesh(TriangularMesh& mesh, string file0D, string file1D, string file2D)
+    {
+
+      if(!ImportCell0Ds(mesh,file0D))
+      {
+        return false;
+      }
+      else
+      {
+        cout << "Cell0D marker:" << endl;
+        for(auto it = mesh.Cell0DMarkers.begin(); it != mesh.Cell0DMarkers.end(); it++)
+        {
+          cout << "key:\t" << it -> first << "\t values:";
+          for(const unsigned int id : it -> second)
+            cout << "\t" << id;
+
+          cout << endl;
+        }
+      }
+
+      if(!ImportCell1Ds(mesh,file1D))
+      {
+        return false;
+      }
+      else
+      {
+        cout << "Cell1D marker:" << endl;
+        for(auto it = mesh.Cell1DMarkers.begin(); it != mesh.Cell1DMarkers.end(); it++)
+        {
+          cout << "key:\t" << it -> first << "\t values:";
+          for(const unsigned int id : it -> second)
+            cout << "\t" << id;
+
+          cout << endl;
+        }
+      }
+
+      if(!ImportCell2Ds(mesh,file2D))
+      {
+        return false;
+      }
+      else
+      {
+        // Test:
+        for(unsigned int c = 0; c < mesh.NumberCell2D; c++)
+        {
+          array<unsigned int, 3> edges = mesh.Cell2DEdges[c];
+
+          for(unsigned int e = 0; e < 3; e++)
+          {
+             const unsigned int origin = mesh.Cell1DVertices[edges[e]][0];
+             const unsigned int end = mesh.Cell1DVertices[edges[e]][1];
+
+             auto findOrigin = find(mesh.Cell2DVertices[c].begin(), mesh.Cell2DVertices[c].end(), origin);
+             if(findOrigin == mesh.Cell2DVertices[c].end())
+             {
+               cerr << "Wrong mesh" << endl;
+               return 2;
+             }
+
+             auto findEnd = find(mesh.Cell2DVertices[c].begin(), mesh.Cell2DVertices[c].end(), end);
+             if(findEnd == mesh.Cell2DVertices[c].end())
+             {
+               cerr << "Wrong mesh" << endl;
+               return 3;
+             }
+
+             cout << "c: " << c << ", origin: " << *findOrigin << ", end: " << *findEnd << endl;
+
+          }
+        }
+      }
+
+      return true;
+
+    }
 }
--- Projects/Raffinamento/src/empty_class.hpp
+++ Projects/Raffinamento/src/empty_class.hpp
@@ -2,14 +2,100 @@
 #define __EMPTY_H
 
 #include <iostream>
+#include "Eigen/Eigen"
+#include <fstream>
+#include "map"
+
+using namespace std;
+using namespace Eigen;
 
 namespace ProjectLibrary
 {
-  class Empty
-  {
-    public:
-      void Show() const { std::cout<< "Hello world;"<< std::endl; }
-  };
+    struct Edge
+    {
+        unsigned int id;
+        Point p1;
+        Point p2;
+    };
+
+    struct Triangle
+    {
+        Point p1;
+        Point p2;
+        Point p3;
+        Edge e1;
+        Edge e2;
+        Edge e3;
+        unsigned int id;
+        double area;
+    };
+
+    struct OrientedEdge
+    {
+      Edge* RealEdge;
+      Triangle* RealTriangle;
+      OrientedEdge* symmetric=nullptr;
+      OrientedEdge* next=nullptr;
+    };
+
+    struct Point
+    {
+        double x;
+        double y;
+        unsigned int id;
+
+        Point(const double& x,
+              const double& y,
+              const unsigned int& id):
+          x(x), y(y), id(id)
+        {
+        }
+
+        Point(const Point& p):
+          x(p.x), y(p.y), id(p.id)
+        {
+        }
+    };
+
+    struct Mesh
+    {
+        unsigned int NumberCell0D = 0; ///< number of Cell0D
+        vector<Point> Cell0D = {}; ///< Cell0D id, size 1 x NumberCell0D
+        map<unsigned int, list<unsigned int>> Cell0DMarkers = {}; ///< Cell0D markers, size 1 x NumberCell0D (marker)
+
+        unsigned int NumberCell1D = 0; ///< number of Cell1D
+        vector<Edge> Cell1D = {}; ///< Cell1D id, size 1 x NumberCell1D
+        map<unsigned int, list<unsigned int>> Cell1DMarkers = {}; ///< Cell1D propertoes, size 1 x NumberCell1D (marker)
+
+        unsigned int NumberCell2D = 0; ///< number of Cell2D
+        vector<Triangle> Cell2DId = {}; ///< Cell2D id, size 1 x NumberCell2D
+
+        vector<OrientedEdge> GraphedMesh;
+    };
+
+    bool clockwise(Point P1, Point P2, Point P3);
+
+    double Area(Triangle triangle);
+
+    ///\brief Import the triangular mesh and test if the mesh is correct
+    ///\param mesh: a TriangularMesh struct
+    ///\return the result of the reading, true if is success, false otherwise
+    bool ImportMesh(Mesh& mesh, string file0D, string file1D, string file2D);
+
+    ///\brief Import the Cell0D properties from Cell0Ds.csv file
+    ///\param mesh: a TriangularMesh struct
+    ///\return the result of the reading, true if is success, false otherwise
+    bool ImportCell0Ds(Mesh& mesh, string nomeFile);
+
+    ///\brief Import the Cell1D properties from Cell1Ds.csv file
+    ///\param mesh: a TriangularMesh struct
+    ///\return the result of the reading, true if is success, false otherwise
+    bool ImportCell1Ds(Mesh& mesh, string nomeFile);
+
+    ///\brief Import the Cell2D properties from Cell2Ds.csv file
+    ///\param mesh: a TriangularMesh struct
+    ///\return the result of the reading, true if is success, false otherwise
+    bool ImportCell2Ds(Mesh& mesh, string nomeFile);
 }
 
 #endif // __EMPTY_H
